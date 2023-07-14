70f7c77a24dd710333f9510bc6a03396b2bcdb05
Yu-Kai Lin <100866406+108590020@users.noreply.github.com>
2023-07-14 11:59:11 +0800

    Movement
    Set player input to move

--- HowToMakeA_VideoGame/Assets/PlayerMovement.cs
+++ HowToMakeA_VideoGame/Assets/PlayerMovement.cs
@@ -6,10 +6,26 @@ public class PlayerMovement : MonoBehaviour
     // This is a reference to the Rigidbody component called "rb"
     public Rigidbody rb;
 
+    public float forwardForce = 2000f;
+    public float sidewayForce = 500f;
+
     // We marked this as "Fixed"Update because we
     // are using it to mess with physics.
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, 2000 * Time.deltaTime); // Add a force of 2000 on the z-axis
+        // Add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime); // Add a force of 2000 on the z-axis
+        
+        if(Input.GetKey("d"))
+        {
+            // Only executed if condition is met
+            rb.AddForce(sidewayForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            // Only executed if condition is met
+            rb.AddForce(-sidewayForce * Time.deltaTime, 0, 0);
+        }
     }
 }
